690c933e1f03a0f5b40d1a9895435e85183f52cd
Halmane <87368529+Halmane@users.noreply.github.com>
2022-01-06 21:59:13 +0400

    end

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include <sstream>
 
 enum power {
-    powerSupply,
-    sockets,
-    lightInside,
-    lightOutside,
-    heating,
-    waterHeating,
-    conditioner
+    powerSupply = 1,
+    sockets = 2,
+    lightInside = 4,
+    lightOutside = 8,
+    heating = 16,
+    waterHeating = 32,
+    conditioner = 64
 };
 
 void stringToBool(std::string &text, bool &status) {
@@ -18,26 +18,54 @@ void stringToBool(std::string &text, bool &status) {
     else status = false;
 }
 
-void waterHeatingSwitch(int& state, float& temperatureOutside) {
+void waterHeatingSwitch(int &state, float &temperatureOutside) {
     if (temperatureOutside < 0 and !(state & waterHeating)) {
         state |= waterHeating;
-        std::cout << "Water heating system: ON"<<std::endl;
+        std::cout << "Water heating system: ON" << std::endl;
     } else if (temperatureOutside > 5 and (state & waterHeating)) {
         state &= ~waterHeating;
-        std::cout << "Water heating system: OFF"<<std::endl;
+        std::cout << "Water heating system: OFF" << std::endl;
     }
 }
 
-void lightOutsideSwitch(int& state, int& hour, bool& movingOutsideStatus ) {
-    if ((hour > 16 or hour < 5) and movingOutsideStatus==1 and !(state & lightOutside)) {
+void lightOutsideSwitch(int &state, int &hour, bool &movingOutsideStatus) {
+    if ((hour > 16 or hour < 5) and movingOutsideStatus == 1 and !(state & lightOutside)) {
         state |= lightOutside;
-        std::cout << "Light outside: ON"<<std::endl;
-    } else if (((hour <= 16 and hour >= 5) or movingOutsideStatus==0)  and (state & lightOutside)) {
+        std::cout << "Light outside: ON" << std::endl;
+    } else if (((hour <= 16 or hour >= 5) and movingOutsideStatus == 0) and (state & lightOutside)) {
         state &= ~lightOutside;
-        std::cout << "Light outside: OFF"<<std::endl;
+        std::cout << "Light outside: OFF" << std::endl;
     }
 }
 
+void heatingSwitch(int &state, float &temperatureInside) {
+    if (temperatureInside <= 22 and !(state & heating)) {
+        state |= heating;
+        std::cout << "Heating: ON" << std::endl;
+    } else if (temperatureInside >= 25 and (state & heating)) {
+        state &= ~heating;
+        std::cout << "Heating: OFF" << std::endl;
+    }
+}
+
+void conditionerSwitch(int &state, float &temperatureInside) {
+    if (temperatureInside >= 30 and !(state & conditioner)) {
+        state |= conditioner;
+        std::cout << "Conditioner: ON" << std::endl;
+    } else if (temperatureInside <= 25 and (state & conditioner)) {
+        state &= ~conditioner;
+        std::cout << "Conditioner: OFF" << std::endl;
+    }
+}
+
+void lightTemperatureInsideSwitch(bool &lightOnInsideStatus, int &hour, int &lightTemperature) {
+    if (lightOnInsideStatus == 1 and (hour >= 15 and hour <= 20)) {
+        std::cout << "Light Temperature Inside: " << lightTemperature << " K" << std::endl;
+        lightTemperature -= 460;
+    } else if (lightOnInsideStatus == 0 and (hour >= 16 and hour <= 20)) {
+        lightTemperature -= 460;
+    }
+}
 
 int main() {
     int hour;
@@ -49,14 +77,15 @@ int main() {
     std::string movingOutsideStatusText;
 
     bool lightOnInsideStatus;
-
     std::string lightOnInsideStatusText;
 
+    int lightTemperature;
 
     int state = 0;
 
     for (int day = 1; day < 2; day++) {
         hour = 0;
+        lightTemperature = 5000;
         for (; hour < maxHour; hour++) {
             std::cout << "Day #" << day << " " << hour << ":00" << std::endl;
             std::cout
@@ -65,8 +94,11 @@ int main() {
             std::cin >> temperatureInside >> temperatureOutside >> movingOutsideStatusText >> lightOnInsideStatusText;
             stringToBool(movingOutsideStatusText, movingOutsideStatus);
             stringToBool(lightOnInsideStatusText, lightOnInsideStatus);
-            waterHeatingSwitch(state,temperatureOutside);
-            lightOutsideSwitch(state, hour,movingOutsideStatus);
+            waterHeatingSwitch(state, temperatureOutside);
+            lightOutsideSwitch(state, hour, movingOutsideStatus);
+            heatingSwitch(state, temperatureInside);
+            conditionerSwitch(state, temperatureInside);
+            lightTemperatureInsideSwitch(lightOnInsideStatus, hour, lightTemperature);
         }
     }
 }
